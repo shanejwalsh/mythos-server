0876e3aa65f5da847bc8dcacbeaa3201f91c089f
Chris Camacho <cacamacho91@gmail.com>
2019-03-21 18:23:11 +0000

    added all endpoints to support random generation and modularised funcitonality

---
+++ app/controllers/api/v1/generators_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::GeneratorsController < ApplicationController
+    include Generator
+
+    def first_name
+      render json: generate_first_name
+    end
+    def last_name
+      render json: generate_last_name
+    end
+    def alias
+      render json: generate_alias
+    end
+    def motto
+      render json: generate_motto
+    end
+    def species
+      render json: generate_species
+    end
+    def bio
+      render json: generate_bio
+    end
+    def alignment
+      render json: generate_alignment
+    end
+    def trait_positive
+      render json: generate_trait_positive
+    end
+    def trait_negative
+      render json: generate_trait_negative
+    end
+    def age
+      render json: generate_age
+    end
+    def status
+      render json: generate_status
+    end
+    def gender
+      render json: generate_gender
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,19 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :characters, only: [:index]
+
+      get '/generate/first_name', to: 'generators#first_name'
+      get '/generate/last_name', to: 'generators#last_name'
+      get '/generate/alias', to: 'generators#alias'
+      get '/generate/motto', to: 'generators#motto'
+      get '/generate/species', to: 'generators#species'
+      get '/generate/bio', to: 'generators#bio'
+      get '/generate/alignment', to: 'generators#alignment'
+      get '/generate/trait_positive', to: 'generators#trait_positive'
+      get '/generate/trait_negative', to: 'generators#trait_negative'
+      get '/generate/age', to: 'generators#age'
+      get '/generate/status', to: 'generators#status'
+      get '/generate/gender', to: 'generators#gender'
     end
   end
 end
